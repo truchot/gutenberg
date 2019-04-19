f71ce9c2d7ef97d7d86a0089d903ec5bcb176fa9
Jorge <jorge.costa@developer.pt>
2019-04-19 12:53:34 +0100

    Connect gutenberg widget screen to widget-area endpoints.

--- packages/edit-widgets/src/components/layout/index.js
+++ packages/edit-widgets/src/components/layout/index.js
@@ -1,9 +1,11 @@
 /**
  * WordPress dependencies
  */
+import { compose } from '@wordpress/compose';
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { navigateRegions } from '@wordpress/components';
+import { withSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -12,13 +14,7 @@ import Header from '../header';
 import Sidebar from '../sidebar';
 import WidgetArea from '../widget-area';
 
-function Layout() {
-	const areas = [
-		__( 'Sidebar' ),
-		__( 'Footer' ),
-		__( 'Header' ),
-	];
-
+function Layout( { areas } ) {
 	return (
 		<Fragment>
 			<Header />
@@ -31,7 +27,7 @@ function Layout() {
 			>
 				{ areas.map( ( area, index ) => (
 					<div key={ index } className="edit-widgets-layout__area">
-						<WidgetArea title={ area } initialOpen={ index === 0 } />
+						<WidgetArea area={ area } initialOpen={ index === 0 } />
 					</div>
 				) ) }
 			</div>
@@ -39,4 +35,13 @@ function Layout() {
 	);
 }
 
-export default navigateRegions( Layout );
+export default compose( [
+	withSelect( ( select ) => {
+		const { getWidgetAreas } = select( 'core/edit-widgets' );
+		const areas = getWidgetAreas();
+		return {
+			areas,
+		};
+	} ),
+	navigateRegions,
+] )( Layout );
