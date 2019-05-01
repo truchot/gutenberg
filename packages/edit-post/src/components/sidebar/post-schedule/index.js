5dfe3e427aa05c5ab8e730b04359b3d48ae6230d
Jorge <jorge.costa@developer.pt>
2019-05-01 23:03:06 +0100

    Fix: Publish date not announced in voice over; Redundant duplicate labels;

--- packages/edit-post/src/components/sidebar/post-schedule/index.js
+++ packages/edit-post/src/components/sidebar/post-schedule/index.js
@@ -5,36 +5,34 @@ import { __ } from '@wordpress/i18n';
 import { PanelRow, Dropdown, Button } from '@wordpress/components';
 import { withInstanceId } from '@wordpress/compose';
 import { PostSchedule as PostScheduleForm, PostScheduleLabel, PostScheduleCheck } from '@wordpress/editor';
-import { Fragment } from '@wordpress/element';
+import { Fragment, renderToString } from '@wordpress/element';
 
 export function PostSchedule( { instanceId } ) {
 	return (
 		<PostScheduleCheck>
 			<PanelRow className="edit-post-post-schedule">
-				<label
-					htmlFor={ `edit-post-post-schedule__toggle-${ instanceId }` }
+				<span
 					id={ `edit-post-post-schedule__heading-${ instanceId }` }
 				>
 					{ __( 'Publish' ) }
-				</label>
+				</span>
 				<Dropdown
 					position="bottom left"
 					contentClassName="edit-post-post-schedule__dialog"
 					renderToggle={ ( { onToggle, isOpen } ) => (
 						<Fragment>
-							<label
-								className="edit-post-post-schedule__label"
-								htmlFor={ `edit-post-post-schedule__toggle-${ instanceId }` }
-							>
-								<PostScheduleLabel /> { __( 'Click to change' ) }
-							</label>
 							<Button
-								id={ `edit-post-post-schedule__toggle-${ instanceId }` }
 								type="button"
 								className="edit-post-post-schedule__toggle"
 								onClick={ onToggle }
 								aria-expanded={ isOpen }
 								aria-live="polite"
+								aria-label={ renderToString(
+									<Fragment>
+										<PostScheduleLabel /> { __( 'Click to change' ) }
+									</Fragment>
+								) }
+								aria-describedby={ `edit-post-post-schedule__heading-${ instanceId }` }
 								isLink
 							>
 								<PostScheduleLabel />
